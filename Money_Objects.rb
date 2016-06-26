47c6202171c13edab1c75d3b42a604b64964a32c
Erik Douglas <eadouglas10@gmail.com>
2016-06-26 15:46:45 -0400

    Added currency trader with initialize method

--- Money_Objects.rb
+++ Money_Objects.rb
@@ -1,12 +1,13 @@
-require_relative './Currency.rb'            # => true
-require_relative './Currency_Converter.rb'  # => true
+require_relative './Currency.rb'
+require_relative './Currency_Converter.rb'
+require_relative './CurrencyTrader'
 
-dollar = Currency.new(5, "USD")  # => #<Currency:0x007f97fc9144e8 @amount=5, @code="USD">
-single = Currency.new(1, "USD")  # => #<Currency:0x007f97fc915dc0 @amount=1, @code="USD">
+dollar = Currency.new(5, "USD")
+single = Currency.new(1, "USD")
 
 
-converter1 = Currency_Converter.new({USD: 1.0, EUR: 0.74})  # => #<Currency_Converter:0x007f97fc916c70 @exchange_rates={:USD=>1.0, :EUR=>0.74}>
+converter1 = Currency_Converter.new({USD: 1.0, EUR: 0.74})
 
-brexit = Currency.new("€40")  # => #<Currency:0x007f97fc917878 @amount=40.0, @code="EUR">
+brexit = Currency.new("€40")
 
-converter1.convert(brexit, "USD")  # => 54.054054054054056
+converter1.convert(brexit, "USD")
